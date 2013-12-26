908093fb0b1bfffbc40d09b1c138df9d87748baf
unknown <chris@.(none)>
2013-12-26 09:40:01 -0800

    Creating Initial Framework to start running application

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'gosu'
+
+
+class GameWindow < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = "Teli Games"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+window = GameWindow.new
+window.show
+
